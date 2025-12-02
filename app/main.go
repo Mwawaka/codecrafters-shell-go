e4240294c0feca411b92e4ec2435623474829259
Mwawaka <kedskeds90@gmail.com>
2025-12-02 11:03:15 +0300

    update: reading input from shell

--- app/main.go
+++ app/main.go
@@ -7,12 +7,17 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
+
 	reader := bufio.NewReader(os.Stdin)
-	command, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "error reading input:", err)
-		os.Exit(1)
+	for {
+		fmt.Print("$ ")
+		command, err := reader.ReadString('\n')
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error reading input:", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s: command not found\n", command[:len(command)-1])
 	}
-	fmt.Printf("%s: command not found\n", command[:len(command)-1])
 }

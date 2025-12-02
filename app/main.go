638240f4b0522214bb1c1864f6d39d82faf6ca15
Mwawaka <kedskeds90@gmail.com>
2025-12-02 10:24:25 +0300

    update: printing error msgs

--- app/main.go
+++ app/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Print("$ ")
+	reader := bufio.NewReader(os.Stdin)
+	command, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s: command not found", command)
 }

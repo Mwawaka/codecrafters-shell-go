52d51abfd84d864091257b58fab0d7eedd8159d5
Mwawaka <kedskeds90@gmail.com>
2025-12-02 13:12:54 +0300

    update: reading input from shell

--- app/main.go
+++ app/main.go
@@ -4,11 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+type CommandHandler func(args []string) (string, error)
+
+var commands = map[string]CommandHandler{
+	"echo": echo,
+}
 
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
+
 	for {
 		fmt.Print("$ ")
 		command, err := reader.ReadString('\n')
@@ -18,10 +26,31 @@ func main() {
 			os.Exit(1)
 		}
 
-		if command[:len(command)-1] == "exit"{
-			os.Exit(0)
+		parts := strings.Split(command[:len(command)-1], " ")
+		cmdName := parts[0]
+
+		if cmdName == "exit" {
+			exit()
 		}
 
-		fmt.Printf("%s: command not found\n", command[:len(command)-1])
+		if handler, exists := commands[cmdName]; exists {
+			out, err := handler(parts[1:])
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "error executing the command:", err)
+				continue
+			}
+			fmt.Println(out)
+			continue
+		}
+
+		fmt.Printf("%s: command not found\n", cmdName)
 	}
+}
+
+func exit() {
+	os.Exit(0)
+}
+
+func echo(args []string) (string, error) {
+	return strings.Join(args, " "),nil
 }
